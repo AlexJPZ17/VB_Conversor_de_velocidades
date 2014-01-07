a6bc828e85115e73ea9e067afee3d8c71b0e4446
AlexJPZ17 <alexjpz17@gmail.com>
2014-01-07 11:32:47 -0600

    VB Conversor de velocidades

---
+++ Conversor de velocidades/Conversor.vb
@@ -0,0 +1,13 @@
+﻿Public Class Conversor
+    Dim velocidad As Double
+    Public Sub Conversor1()
+        Console.WriteLine("Introduzca una velocidad en m/s")
+        velocidad = Convert.ToDouble(Console.ReadLine())
+        Console.WriteLine(velocidad & " m/s = " & velocidad * 3600 / 1000 & " km/h")
+    End Sub
+    Public Sub Conversor2()
+        Console.WriteLine("Introduzca una velocidad en km/h")
+        velocidad = Convert.ToDouble(Console.ReadLine())
+        Console.WriteLine(velocidad & " km/h = " & velocidad * 1000 / 3600 & " m/s")
+    End Sub
+End Class
---
+++ Conversor de velocidades/Module1.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim opcion, repetir As Integer
+        Dim conv = New Conversor()
+        Do
+            Console.WriteLine("Escoja una de las opciones")
+            Console.WriteLine("1 - Convertir m/s a km/h")
+            Console.WriteLine("2 - Convertir km/h a m/s")
+            opcion = Convert.ToInt32(Console.ReadLine())
+            Select Case opcion
+                Case 1
+                    conv.Conversor1()
+                Case 2
+                    conv.Conversor2()
+                Case Else
+                    Console.WriteLine("Opción errónea")
+            End Select
+            Console.WriteLine("Si desea repetir coloque el valor de 1 o cualquier tecla para salir")
+            repetir = Convert.ToInt32(Console.ReadLine())
+        Loop While (repetir = 1)
+    End Sub
+
+End Module
